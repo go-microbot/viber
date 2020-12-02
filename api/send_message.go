d4fc7651a013af4927661f5c078d7ff210331ad0
MonkeyBuisness <gutorov.artem@yandex.ru>
2020-12-02 11:37:36 +0300

    add more methods

--- api/send_message.go
+++ api/send_message.go
@@ -19,6 +19,49 @@ func (api *ViberAPI) SendPictureMessage(ctx context.Context, req apiModels.SendP
 	return api.sendMessage(ctx, req)
 }
 
+// SendVideoMessage represents method to send video message.
+func (api *ViberAPI) SendVideoMessage(ctx context.Context, req apiModels.SendVideoMessageRequest) (*apiModels.MessageResponse, error) {
+	req.Type = models.MessageTypeVideo
+	return api.sendMessage(ctx, req)
+}
+
+// SendFileMessage represents method to send file message.
+func (api *ViberAPI) SendFileMessage(ctx context.Context, req apiModels.SendFileMessageRequest) (*apiModels.MessageResponse, error) {
+	req.Type = models.MessageTypeFile
+	return api.sendMessage(ctx, req)
+}
+
+// SendContactMessage represents method to send contact message.
+func (api *ViberAPI) SendContactMessage(ctx context.Context, req apiModels.SendContactMessageRequest) (*apiModels.MessageResponse, error) {
+	req.Type = models.MessageTypeContact
+	return api.sendMessage(ctx, req)
+}
+
+// SendLocationMessage represents method to send location message.
+func (api *ViberAPI) SendLocationMessage(ctx context.Context, req apiModels.SendLocationMessageRequest) (*apiModels.MessageResponse, error) {
+	req.Type = models.MessageTypeLocation
+	return api.sendMessage(ctx, req)
+}
+
+// SendURLMessage represents method to send URL message.
+func (api *ViberAPI) SendURLMessage(ctx context.Context, req apiModels.SendURLMessageRequest) (*apiModels.MessageResponse, error) {
+	req.Type = models.MessageTypeURL
+	return api.sendMessage(ctx, req)
+}
+
+// SendStickerMessage represents method to send sticker message.
+func (api *ViberAPI) SendStickerMessage(ctx context.Context, req apiModels.SendStickerMessageRequest) (*apiModels.MessageResponse, error) {
+	req.Type = models.MessageTypeSticker
+	return api.sendMessage(ctx, req)
+}
+
+// SendRichMediaMessage represents method to send rich media message.
+func (api *ViberAPI) SendRichMediaMessage(ctx context.Context, req apiModels.SendRichMediaMessageRequest) (*apiModels.MessageResponse, error) {
+	req.Type = models.MessageTypeRichMedia
+	req.RichMedia.Type = string(models.MessageTypeRichMedia)
+	return api.sendMessage(ctx, req)
+}
+
 func (api *ViberAPI) sendMessage(ctx context.Context, req interface{}) (*apiModels.MessageResponse, error) {
 	resp, err := api.NewRequest("send_message").
 		Body(NewJSONBody(req)).
